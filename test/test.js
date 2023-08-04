bce475d9e2a7ed6361ea01711a048b46c06081fc
ZORG23 <alex.cherev@mail.ru>
2023-08-04 09:54:51 +0300

    фыв

--- test/test.js
+++
@@ -1,9 +0,0 @@
-describe('Проверка сайта Ситилинк ', () => {
-
-    it('should have right title - the fancy generator way', () => {
-        browser.url('https://www.citilink.ru/?_action=autologin&_success_login=1');
-        browser.waitForVisible('h3.e114sczy0');
-    }
-
-    )
-})
